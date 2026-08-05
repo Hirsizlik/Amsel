a802e7049dcaf9f2fc88a218d1a40d89d11dae65
Markus Schagerl <Markus.Schagerl1@gmx.at>
2026-08-05 11:57:03 +0200

    ArenaState: LoadCardInfo to async

--- src/Amsel.Blazor/Components/ArenaState.cs
+++ src/Amsel.Blazor/Components/ArenaState.cs
@@ -7,12 +7,15 @@ public sealed class ArenaState
     public Dictionary<uint, CardInfo> Cards { get; private set; } = [];
     public Dictionary<uint, CardOwned> CardsOwned { get; private set; } = [];
 
-    public void LoadCardInfo()
+    public async Task LoadCardInfoAsync()
     {
-        IMtgArenaConnect connect = new MtgArenaConnect();
-        using IMtgArenaDatabase db = new MtgArenaDatabase(connect.GetDatabasePath());
-        var loc = db.GetEnglishLocalization();
-        Cards = db.GetAllCards(loc);
-        CardsOwned = connect.GetCardsOwnedFromInventory();
+        await Task.Run(() =>
+        {
+            IMtgArenaConnect connect = new MtgArenaConnect();
+            using IMtgArenaDatabase db = new MtgArenaDatabase(connect.GetDatabasePath());
+            var loc = db.GetEnglishLocalization();
+            Cards = db.GetAllCards(loc);
+            CardsOwned = connect.GetCardsOwnedFromInventory();
+        });
     }
 }
